758658209625e201b8da80bc4823d77330018be6
Tuan Anh Nguyen <atngu312@gmail.com>
2023-04-29 00:01:15 -0400

    Array exercises

---
+++ looping-range.js
@@ -0,0 +1,19 @@
+let range = function(start, end, step) {
+  let output = [];
+  if(start > end || step <= 0 || start === undefined || end === undefined || step === undefined) {
+    return output;
+  } else {
+    for(let i = start; i <= end; i += step) {
+      output.push(i);
+    }
+    return output;
+  }
+}
+
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
+console.log(range(0, 10, 0));
+console.log(range(12, 10, -2));
+
